cb74134eeb628d5c48a738b169c8359d5b855f0c
Mathias Bynke <mathias@bynke.no>
2018-10-03 16:27:31 +0200

    Opprettet TodoList-komponenten
    Samt lagt inn en ScrollView

---
+++ reactnativedemo/TodoList.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
+export default class TodoList extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          {/* Jeg klarte ikke å få dette til å bli generert i løkke */}
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+          <Text>Listeoppføring</Text>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#888",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
