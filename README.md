6ea07154a36b66fbcf1e679dbc5f17d1d937d62f
Mathias Bynke <mathias@bynke.no>
2018-10-04 11:54:09 +0200

    Løkkegenerering av listeelementer + styling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # it2810-webutvikling-h18-prosjekt-3-24
+
+Kilde: https://tutorialscapital.com/react-native-adding-items-in-scrollview-using-loop-method-android-ios-tutorial-from-scratch/
--- reactnativedemo/TodoList.js
+++ reactnativedemo/TodoList.js
@@ -2,69 +2,54 @@ import React from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.items = [];
+    for (i = 0; i < 60; i++) this.items.push("Listeoppføring");
+    console.log("this.items i TodoList:", this.items);
+  }
+
+  scrollViewArray() {
+    return this.items.map((item, key) => (
+      <View key={key} style={styles.item}>
+        <Text style={styles.text}>
+          Oppføring nummer {key} – {item}
+        </Text>
+        <View style={styles.separator} />
+      </View>
+    ));
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView>
-          {/* Jeg klarte ikke å få dette til å bli generert i løkke */}
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-          <Text>Listeoppføring</Text>
-        </ScrollView>
+      <View style={styles.todoList}>
+        <ScrollView>{this.scrollViewArray()}</ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  todoList: {
     flex: 1,
-    backgroundColor: "#888",
+    backgroundColor: "#111",
     alignItems: "center",
     justifyContent: "center"
+  },
+
+  separator: {
+    height: 1,
+    marginTop: 2,
+    marginBottom: 2,
+    backgroundColor: "#eee"
+  },
+
+  item: {
+    marginLeft: 8,
+    marginRight: 8
+  },
+
+  text: {
+    color: "white"
   }
 });
