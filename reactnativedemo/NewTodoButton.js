67def25dd36b7b66a9503477740eb86d19c04dea
Mathias Bynke <mathias@bynke.no>
2018-10-04 14:33:00 +0200

    Implementert Nativebase

--- reactnativedemo/NewTodoButton.js
+++ reactnativedemo/NewTodoButton.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Button, Text } from "native-base";
 
 export default class NewTodoButton extends React.Component {
   render() {
-    return <Button title="Nytt gjøremål" onPress={this.props.onPress} />;
+    return (
+      <Button title="Nytt gjøremål" onPress={this.props.onPress} full>
+        <Text>Nytt gjøremål</Text>
+      </Button>
+    );
   }
 }
