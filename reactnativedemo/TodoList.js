b9143a4a5a5af2ac6d8200155a2c193781fea433
Mathias Bynke <mathias@bynke.no>
2018-10-04 15:34:16 +0200

    Byttet ut ScrollView med List
    Samt fjernet nå unødvendig styling

--- reactnativedemo/TodoList.js
+++ reactnativedemo/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { List, ListItem, Text } from "native-base";
 
 import TodoListItem from "./TodoListItem";
 import NewTodoButton from "./NewTodoButton";
@@ -14,47 +15,20 @@ export default class TodoList extends React.Component {
 
   scrollViewArray() {
     return this.items.map((item, key) => (
-      <View key={key} style={styles.item}>
+      <ListItem key={key}>
         <TodoListItem />
-        <View style={styles.separator} />
-      </View>
+      </ListItem>
     ));
   }
 
   render() {
     return (
-      <View style={styles.todoList}>
+      <View>
         <NewTodoButton
           onPress={() => console.log("Nytt gjøremål-knapp trykt!")}
         />
-        <ScrollView>{this.scrollViewArray()}</ScrollView>
+        <List>{this.scrollViewArray()}</List>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  todoList: {
-    flex: 1,
-    width: "100%", // La listen fylle hele bredden av appen.
-    backgroundColor: "#111",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-
-  separator: {
-    height: 1,
-    marginTop: 2,
-    marginBottom: 2,
-    backgroundColor: "#eee"
-  },
-
-  item: {
-    marginLeft: 8,
-    marginRight: 8
-  },
-
-  text: {
-    color: "white"
-  }
-});
--- reactnativedemo/TodoListItem.js
+++ reactnativedemo/TodoListItem.js
@@ -1,14 +1,8 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Text } from "native-base";
 
 export default class TodoListItem extends React.Component {
   render() {
-    return <Text style={styles.text}>Gjøremål</Text>;
+    return <Text>Gjøremål</Text>;
   }
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: "white"
-  }
-});
