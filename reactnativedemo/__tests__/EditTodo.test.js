929d31a6bf16f3a2d9256e2ae278d2e92b465f4b
Mathias Bynke <mathias@bynke.no>
2018-10-11 16:08:03 +0200

    Gjort EditTodo-testen litt mindre ødelagt

--- reactnativedemo/__tests__/EditTodo.test.js
+++ reactnativedemo/__tests__/EditTodo.test.js
@@ -2,6 +2,12 @@ import React from "react";
 import EditTodo from "../components/EditTodo";
 import renderer from "react-test-renderer";
 
+console.error = jest.fn();
+
+beforeEach(() => {
+  console.error.mockClear();
+});
+
 test("blir tegnet riktig", () => {
   const tree = renderer.create(<EditTodo />).toJSON();
   expect(tree).toMatchSnapshot();
