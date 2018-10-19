f1b91969b5faa7647f51ceb1e576e9fa90f1b120
Mathias Bynke <mathias@bynke.no>
2018-10-19 14:54:21 +0200

    Implementert framgangsviser

--- components/StepCounter.js
+++ components/StepCounter.js
@@ -1,5 +1,7 @@
 import { Pedometer } from 'expo';
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import ProgressBar from 'react-native-progress/Bar';
 import { Text, View } from 'native-base';
 
 export default class StepCounter extends React.Component {
@@ -7,6 +9,7 @@ export default class StepCounter extends React.Component {
     isPedometerAvailable: 'checking',
     todayStepCount: 0,
     currentStepCount: 0,
+    goalStepCount: 10000,
   };
 
   componentDidMount() {
@@ -58,10 +61,29 @@ export default class StepCounter extends React.Component {
   };
 
   render() {
+    const progress = Math.max(0, Math.min(1, this.state.todayStepCount / this.state.goalStepCount));
+    const red = 255 * Math.max(0, Math.min(1, 2 * (1 - progress)));
+    const green = 255 * Math.max(0, Math.min(1, 2 * progress));
+    console.log('progress: ' + progress + ', red: ' + red + ', green: ' + green);
     return (
-      <View>
-        <Text>Du har gått {this.state.todayStepCount} skritt i dag</Text>
+      <View style={styles.counter}>
+        <Text>
+          Du har gått {this.state.todayStepCount} av {this.state.goalStepCount} skritt i dag.
+        </Text>
+        <ProgressBar
+          progress={progress}
+          width={null}
+          height={12}
+          color={'rgba(' + red + ', ' + green + ', 0, 1)'}
+          borderColor="rgba(0, 0, 0, 1)"
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  counter: {
+    marginBottom: 12,
+  },
+});
